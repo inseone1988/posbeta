acb8d01a9e273f8e50e9e4149edc720c514c0fd9
Javier Ramirez <jimlombriz1988@gmail.com>
2021-10-28 15:03:25 -0500

    Changed ticket web print to URI open dialog

--- POSBETA/ticket.php
+++ POSBETA/ticket.php
@@ -16,75 +16,13 @@ if (isset($_GET["orderid"])) {
     <link href="css/print.css" rel="stylesheet"/>
 </head>
 <body>
-<div class="text-center"><img class="img-logo" src="img/logo.jpg"></div>
-<h3>Casa de la copia</h3>
-<div class="text-center" style="font-size: 0.8rem;"><?php echo $_SESSION["branch_data"]["branch_address"]; ?></div>
-<p style="font-size: 0.7rem;"> TELEFONO : <?php echo $_SESSION["branch_data"]["branch_phone"]; ?></p>
-<p style="font-size: 0.7rem;"><?php echo "RFC : " . $_SESSION["branch_data"]["branch_RFC"]; ?></p>
-<p id="datetime"></p>
-<p class="folio">Folio No : <?php echo $order["order"][0]["OrderId"] ?></p>
-<table>
-    <thead>
-    <tr>
-        <th style="width: 60%;">Descripcion</th>
-        <th style="width: 10%;">Cant</th>
-        <th style="width: 10%;">Precio</th>
-        <th style="width: 10%;">Total</th>
-    </tr>
-    <tbody>    <?php for ($i = 0; $i < count($order["orderdetails"]); $i++) {
-        $itemTotalMoney = (floatval($order["orderdetails"][$i]["Quantity"]) * floatval($order["orderdetails"][$i]["price"]));
-        $itemTotal = "$" . number_format((floatval($order["orderdetails"][$i]["Quantity"]) * floatval($order["orderdetails"][$i]["price"])), 2);
-        $gTotal += $itemTotalMoney;
-        echo sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $order["orderdetails"][$i]["Name"], $order["orderdetails"][$i]["Quantity"], "$" . $order["orderdetails"][$i]["price"], $itemTotal);
-    } ?>    </tbody>
-    <tfoot>
-    <tr>
-        <th></th>
-        <th>IVA</th>
-        <th colspan="2"><?php echo "$" . $order["payment"][0]["IVA"] ?></th>
-    </tr>
-    <tr>
-        <th></th>
-        <th>Total</th>
-        <th colspan="2"> <?php echo "$" . $gTotal ?></th>
-    </tr>
-    <?php
+    <div>Hello</div>
 
-    if (count($order["payment"]) > 1) {
-        for ($i = 0; $i < count($order["payment"]); $i++) {
-            if ($order["payment"][$i]["payment"] <= $order["payment"][$i]["Total"]){
-                echo "<tr><th></th><th>Anticipo[" . ($i + 1) . "]</th><th colspan='2'> $" . $order["payment"][$i]["payment"] . "</th></tr>";
-            }else{
-                break;
-            };
-
-        }
-        echo "<tr><th></th><th>Credito</th><th>$" . $order["payment"][(count($order["payment"]) - 1)]["Credit"] . "</th>";
-    }
-
-    ?>
-    <tr>
-        <th></th>
-        <th>Pago</th>
-        <th colspan="2"><?php echo "$" . $order["payment"][(count($order["payment"]) - 1)]["payment"] ?></th>
-    </tr>
-    <tr>
-        <th></th>
-        <th>Cambio</th>
-        <th colspan="2"><?php echo "$" . $order["payment"][0]["change"] ?></th>
-    </tr>
-    </tfoot>
-    </thead></table>
-<p>Gracias por su compra</p>
-<script src="../node_modules/jquery/dist/jquery.min.js"></script>
-<script src="../node_modules/moment/min/moment-with-locales.js"></script>
-<script>
-    moment.locale('es');
-    $("#datetime").text(displayCurrentHour());
-
-    function displayCurrentHour() {
-        return moment().format("LLLL");
-    }
-</script>
 </body>
+
+<script>
+    var order = <?php echo json_encode($order)?> ;
+    window.open("vialogikprint:" + btoa(JSON.stringify(order)),"_blank");
+</script>
+
 </html>
